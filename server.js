22f268696be98da5537fe5248665bbe533a3bf22
estevanmaito <ejmaito@gmail.com>
2016-01-05 20:05:56 -0200

    change server port

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ var routes = require('./app/routes/index.js');
 
 var app = express();
 
+var port = process.env.PORT || 5000;
+
 routes(app);
 
-app.listen(3000, function() {
-	console.log('Listening on port 3000...');
+app.listen(port, function() {
+  console.log('Node app is running on port ', port);
 });
+
+// app.listen(3000, function() {
+// 	console.log('Listening on port 3000...');
+// });
